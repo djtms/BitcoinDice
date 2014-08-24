14d1f7ad2301f4e940f4bd9b0721c864013db0a5
Tomasz Adamski <djtms84@gmail.com>
2014-08-24 13:31:21 +0200

    Update news.php
    Spolszczenie

--- admin/pages/news.php
+++ admin/pages/news.php
@@ -11,29 +11,29 @@ if (!isset($included)) exit();
 if (isset($_POST['new_new'])) {
   if (!empty($_POST['new_new'])) {
     mysql_query("INSERT INTO `news` (`content`) VALUES ('".prot($_POST['new_new'])."')");
-    echo '<div class="zpravagreen"><b>Success:</b> New has been posted.</div>';
+    echo '<div class="zpravagreen"><b>Powodzenie:</b> Wiadomość została wysłana.</div>';
   }
-  else echo '<div class="zpravared"><b>Error:</b> One of required fields stayed empty.</div>';
+  else echo '<div class="zpravared"><b>Błąd:</b> Jedno z wymaganych pól zostało puste.</div>';
 }  
 ?>
 <h1>News</h1>
 <div class="zprava">
-<b>Add new:</b><br>
+<b>Dodaj newsa:</b><br>
 <form method="post" action="./?p=news">
 <table><tr><td><textarea name="new_new" style="width: 480px; height: 70px;"></textarea></td><td><input style="padding: 10px;" type="submit" value="Post"></td></tr></table>
 <small>
-<b>[B]</b>...<b>[/B]</b> = bold font<br>
-<b>[I]</b>...<b>[/I]</b> = italic font<br>
-<b>[BR]</b> = new line
+<b>[B]</b>...<b>[/B]</b> = Czcionka pogrubiona <br>
+<b>[I]</b>...<b>[/I]</b> = Czcionka kursywą<br>
+<b>[BR]</b> = nowy wiersz
 </small>
 </form>
 </div>
 <table class="vypis_table">
   <tr class="vypis_table_head">
     <th>ID</th>
-    <th>Time</th>
-    <th>Content</th>
-    <th>Actions</th>
+    <th>Czas</th>
+    <th>Treść</th>
+    <th>Czynności</th>
   </tr>
   <?php
   $qu=mysql_query("SELECT * FROM `news` ORDER BY `time` DESC");
@@ -42,20 +42,20 @@ if (isset($_POST['new_new'])) {
     echo '<td>'.$row['id'].'</td>';
     echo '<td><small>'.$row['time'].'</small></td>';
     echo '<td class="content'.$row['id'].' getwidth">'.$row['content'].'</td>';
-    echo '<td><a href="#" onclick="javascript:removeNew('.$row['id'].');return false;" title="Delete"><img src="./imgs/cross.png" style="width: 16px;"></a>&nbsp;<a href="#" onclick="javascript:edit_new('.$row['id'].',$(\'td.content'.$row['id'].'\').html());return false;" title="Edit New" id="edit_karos"><img src="./imgs/edit.png" style="width: 16px;"></a></td>';
+    echo '<td><a href="#" onclick="javascript:removeNew('.$row['id'].');return false;" title="Usuń"><img src="./imgs/cross.png" style="width: 16px;"></a>&nbsp;<a href="#" onclick="javascript:edit_new('.$row['id'].',$(\'td.content'.$row['id'].'\').html());return false;" title="Edytuj newsa" id="edit_karos"><img src="./imgs/edit.png" style="width: 16px;"></a></td>';
     echo '</tr>';
   }
   ?>
 </table>
 <script type="text/javascript">
   function removeNew(id) {
-    if (confirm('Do you really want to delete this new?')) {
+    if (confirm('Czy na pewno chcesz usunąć tą wiadomość?')) {
       $.ajax({
         'url': 'ajax/delete_new.php?_new='+id,
         'dataType': "json",
         'success': function(data) {
           $("tr#rowid_"+id).remove();
-          message('success','New has been deleted.');
+          message('powodzenie','News został usunięty.');
         }
       });
     }
@@ -66,7 +66,7 @@ if (isset($_POST['new_new'])) {
       'dataType': "json",
       'success': function(data) {
         $("td.content"+id).html(content);
-        message('success','New has been edited.');
+        message('powodzenie','News został zmodyfikowany.');
       }
     }); 
   }
